26799499d747367debb4c8da45b9bcf8e91eff77
Pratham131 <104488785+Pratham131@users.noreply.github.com>
2023-07-20 17:31:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function getFormvalue() {
     //Write your code here
-	const firstName = document.getElementByTagName("input")[0].value
-	const lastName = document.getElementByTagName("input")[1].value
-
+	let form = document.getElementById("form1")
+	
+	const firstName = form.fname.value
+	const lastName = form.lname.value
 	alert(`${firstName} ${lastName}`)
 }
 
-document.getElementById("form1").addEventListner("submit", function(event){
-	event.preventDefault();
-	getFormvalue();
-})
+// document.getElementById("form1").addEventListner("submit", function(event){
+// 	event.preventDefault();
+// 	getFormvalue();
+// })  
